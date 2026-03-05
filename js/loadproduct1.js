1e3d2a80c538510417c48fe02e5e15c0d1dfe082
NghiaLe <nghiale.f.lab@gmail.com>
2026-03-06 00:01:45 +0700

    Create loadproduct1.js

---
+++ js/loadproduct1.js
@@ -0,0 +1,45 @@
+/* ======================================
+AUTO LOAD PROJECTS
+====================================== */
+
+fetch("/models/projects.json")
+
+.then(response => response.json())
+
+.then(projects => {
+
+const grid = document.getElementById("projectGrid")
+
+projects.forEach(project => {
+
+fetch(`/models/${project.slug}/info.json`)
+
+.then(response => response.json())
+
+.then(data => {
+
+const item = document.createElement("a")
+
+item.href = `product.html?project=${project.slug}`
+
+item.innerHTML = `
+
+<div class="project">
+
+<img src="/models/${project.slug}/${data.media.thumbnail}">
+
+<div class="project-title">
+${data.info.title}
+</div>
+
+</div>
+
+`
+
+grid.appendChild(item)
+
+})
+
+})
+
+})
